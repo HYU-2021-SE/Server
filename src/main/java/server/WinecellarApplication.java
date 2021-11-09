a6996e81bddc15da739264ae5560832930d83cac
yerincho <forjustice9839@gmail.com>
2021-11-09 22:27:01 +0900

    init project

---
+++ src/main/java/server/WinecellarApplication.java
@@ -0,0 +1,13 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinecellarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinecellarApplication.class, args);
+    }
+
+}
