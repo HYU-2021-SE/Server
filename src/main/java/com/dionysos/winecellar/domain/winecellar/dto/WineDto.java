ebe4359ea60ed740eb5a8b3b081791b3b1f74469
yerincho <forjustice9839@gmail.com>
2021-12-06 18:03:41 +0900

    refactor: refactor location value

--- src/main/java/com/dionysos/winecellar/domain/winecellar/dto/WineDto.java
+++ src/main/java/com/dionysos/winecellar/domain/winecellar/dto/WineDto.java
@@ -1,7 +1,6 @@
 package com.dionysos.winecellar.domain.winecellar.dto;
 
 import com.dionysos.winecellar.domain.wine.domain.Wine;
-import com.dionysos.winecellar.domain.wine.domain.Wine.Location;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,11 +9,10 @@ import lombok.Getter;
 public class WineDto {
     private Long wineId;
     private String wineName;
-    private Location location;
+    private Integer location;
     private String labelImage;
 
     public static WineDto from(Wine wine) {
-        System.out.println(wine.toString());
-        return new WineDto(wine.getWineId(), wine.getWineName(), wine.getLocation(), wine.getLabelImage());
+        return new WineDto(wine.getWineId(), wine.getWineName(), wine.getLocation().getLocation(), wine.getLabelImage());
     }
 }
